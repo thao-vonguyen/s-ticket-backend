f0eaf51e682d81ad8979671d4903969ed18d80ca
dotiennee <149573030+dotiennee@users.noreply.github.com>
2024-12-15 02:17:30 +0700

    e

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,16 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { TicketService } from './ticket.service';
-import { CreateTicketDto } from './dto/create-ticket.dto';
-import { UpdateTicketDto } from './dto/update-ticket.dto';
 
 @Controller('ticket')
 export class TicketController {
-  constructor(private readonly ticketService: TicketService) {}
+  constructor(private readonly ticketService: TicketService) { }
 
   @Post()
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketService.create(createTicketDto);
-  }
+  // create(@Body() createTicketDto: CreateTicketDto) {
+  //   return this.ticketService.create(createTicketDto);
+  // }
 
   @Get()
   findAll() {
@@ -22,10 +20,10 @@ export class TicketController {
     return this.ticketService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
-    return this.ticketService.update(+id, updateTicketDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
+  //   return this.ticketService.update(+id, updateTicketDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
