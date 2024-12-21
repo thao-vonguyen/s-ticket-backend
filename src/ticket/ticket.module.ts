3275d44fd6764a60cfbc92f362369e30ba063237
dotiennee <149573030+dotiennee@users.noreply.github.com>
2024-12-21 15:24:19 +0700

    Inclide MiniEventWithTicket

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -3,7 +3,7 @@ import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
-import { MiniEvent } from 'src/mini-event/entities/mini-event.entity';
+import { MiniEvent, MiniEventWithTicketRank } from 'src/mini-event/entities/mini-event.entity';
 import { MiniEventService } from 'src/mini-event/mini-event.service';
 import { EventService } from 'src/event/event.service';
 import { TicketRank } from 'src/ticket-rank/entities/ticket-rank.entity';
@@ -11,7 +11,7 @@ import { TicketRankService } from 'src/ticket-rank/ticket-rank.service';
 import { Event } from 'src/event/entities/event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket, MiniEvent, TicketRank, Event])],
+  imports: [TypeOrmModule.forFeature([Ticket, MiniEvent, TicketRank, Event, MiniEventWithTicketRank])],
   controllers: [TicketController],
   providers: [TicketService, MiniEventService, EventService, TicketRankService],
 })
--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { TicketRank } from 'src/ticket-rank/entities/ticket-rank.entity';
 import { Ticket } from 'src/ticket/entities/ticket.entity';
-import { MiniEvent } from 'src/mini-event/entities/mini-event.entity';
+import { MiniEvent, MiniEventWithTicketRank } from 'src/mini-event/entities/mini-event.entity';
 import { TicketRankService } from 'src/ticket-rank/ticket-rank.service';
 import { TicketService } from 'src/ticket/ticket.service';
 import { MiniEventService } from 'src/mini-event/mini-event.service';
@@ -15,7 +15,7 @@ import { Event } from 'src/event/entities/event.entity';
 import { EventService } from 'src/event/event.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, TicketRank, Ticket, MiniEvent, Payment, Event])],
+  imports: [TypeOrmModule.forFeature([Transaction, TicketRank, Ticket, MiniEvent, Payment, Event, MiniEventWithTicketRank])],
   controllers: [TransactionController],
   providers: [TransactionService, TicketRankService, TicketService, MiniEventService, PaymentService, EventService],
 })
